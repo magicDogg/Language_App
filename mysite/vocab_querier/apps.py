7da2ee380c5be51e3279d53d358fa5f863d5f619
magicdogg <chyyl731@gmail.com>
2021-07-06 22:23:54 -0400

    Create apps based on our task

---
+++ mysite/vocab_querier/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VocabQuerierConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vocab_querier'
---
+++ mysite/vocab_recorder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VocabRecorderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vocab_recorder'
