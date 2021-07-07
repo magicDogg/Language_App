61ff2d80ce6b74876059368d4cf409f9e73fc71b
magicdogg <chyyl731@gmail.com>
2021-07-06 21:58:40 -0400

    Clean up directories + Create new apps based on our task

--- mysite/second_module/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SecondModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'second_module'
--- mysite/second_module/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello world. This is the index for the 'Second_Module'")
